e3addb51f9ed33fade7221dfe0a970eac4de31de
Chris <c.narr@tum.de>
2026-07-05 22:39:04 +0200

    Quartz sync: Jul 5, 2026, 10:39 PM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,17 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
 
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
+
+
+ExternalPlugin.Explorer({
+  mapFn: (node) => {
+    if (node.isFolder) {
+      node.displayName = "📁 " + node.displayName
+    } else {
+      node.displayName = "📄 " + node.displayName
+    }
+  },
+})
