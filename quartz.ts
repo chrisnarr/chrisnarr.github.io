aa01566b4daca47954b7f73022fee3235b13fbf0
Chris <c.narr@tum.de>
2026-07-19 19:28:23 +0200

    Update

--- quartz.ts
+++ quartz.ts
@@ -1,10 +1,6 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
 
-const config = await loadQuartzConfig()
-export default config
-export const layout = await loadQuartzLayout()
-
 ExternalPlugin.Explorer({
   mapFn: (node) => {
     if (node.isFolder) {
@@ -14,4 +10,8 @@ ExternalPlugin.Explorer({
     }
   },
 })
+
+const config = await loadQuartzConfig()
+export default config
+export const layout = await loadQuartzLayout()
 
